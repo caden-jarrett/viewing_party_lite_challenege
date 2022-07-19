6ba2aa0f0e4c841f786c23fcd2a7b091b67bec34
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-19 12:43:49 -0600

    Docs: refactored login controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def login 
     user = User.find_by(username: params[:username])
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
       redirect_to "/users/#{user.id}"
